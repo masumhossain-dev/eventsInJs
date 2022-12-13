29ad0e6cb86f319c39370a2f90ac98782e073daf
MasumHossain71 <masumhazi311@gmail.com>
2022-12-13 14:28:20 +0600

    change inner html using addEventListener

--- eventSummery/script.js
+++ eventSummery/script.js
@@ -1,4 +1,9 @@
 function clickToChange(){
    let clicker = document.getElementById('click-me');
-   clicker.innerHTML = `This is a concept is, "How to change a value using JS event Handler".`;
+   clicker.innerHTML = `This is onclick`;
 }
+
+document.getElementById('event-listener').addEventListener('click', function(){
+   let listenver = document.getElementById('click-me');
+   listenver.innerHTML = 'This is Event Listener';
+})
