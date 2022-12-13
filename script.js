306360cfa6b39cfd9dbaef3802e22889c65c7836
MasumHossain71 <masumhazi311@gmail.com>
2022-12-13 13:51:16 +0600

    this is javascript event handelling repo

---
+++ script.js
@@ -0,0 +1,34 @@
+// option One 
+// {/* <button onclick="document.body.style.backgroundColor = 'red'">Red</button>  */}
+
+// option Two 
+function colorBlue (){
+  document.body.style.backgroundColor = 'blue';
+}
+
+// option Three 
+let makeGreen = document.getElementById('make-green');
+makeGreen.onclick = makeGreenColor;
+
+function makeGreenColor (){
+  document.body.style.backgroundColor = 'green';
+}
+
+// option Three Another
+let makePurple = document.getElementById('make-purple');
+makePurple.onclick = function makePurpleColor(){
+  document.body.style.backgroundColor = 'purple';
+}
+
+// option Four
+let makePink = document.getElementById('make-pink');
+makePink.addEventListener('click', makePinkColor);
+function makePinkColor (){
+  document.body.style.backgroundColor = 'pink';
+}
+
+// option Four another
+let makeYellow = document.getElementById('make-yellow');
+makeYellow.addEventListener('click', function makeYellowColor(){
+  document.body.style.backgroundColor = 'yellow';
+})
