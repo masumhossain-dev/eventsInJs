5c3e11ad7da75cb0b5e2a530a2b238c5f91a48c8
MasumHossain71 <masumhazi311@gmail.com>
2022-12-13 14:55:02 +0600

    input value to change value using callback function

--- eventSummery/script.js
+++ eventSummery/script.js
@@ -7,3 +7,11 @@ document.getElementById('event-listener').addEventListener('click', function(){
    let listenver = document.getElementById('click-me');
    listenver.innerHTML = 'This is Event Listener';
 })
+
+document.getElementById('btn-update').addEventListener('click', function(){
+   let inputField = document.getElementById('input-field');
+   let inputText = inputField.value;
+   let p = document.getElementById('click-me');
+   p.innerText = inputText;
+   inputField.value = '';
+})
